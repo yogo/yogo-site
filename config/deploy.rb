1876e75aef33d12d0f78acfd4ee55786135eb994
Pol Llovet <pol.llovet@gmail.com>
2010-10-18 11:16:28 -0600

    deploy file now works to deploy output to optimus

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,25 @@
-# set :application,   "voeis-demo"
-# set :repository,    "https://caJaeger@github.com/yogo/yogo-site.git"
-# set :apps_database, "#{application}_production"
-# set :dev_database,  "#{application}_development"
+# Uncomment these if you want to try using cap's native copy
+# set :repository,  "./output"
+# set :scm, :none
+# set :deploy_via, :copy
+set :deploy_to, "/var/www/yogo.msu.montana.edu"
+set :user,      "web_user"
+set :server_url, "optimus.msu.montana.edu"
+set :use_sudo,  false
 
-desc "Common Settings"
-task :common_settings do
-  set :deploy_to, "//var/www/yogo-test"
-  set :user,      "web_user"
-  set :use_sudo,  false
+role :app, server_url
 
-  server domain, :app, :web
-end
-
-# desc "Deploy to dev.msu.montana.edu"
-# task :dev do
-#   set :user, "web_user"
-#   set :domain, "optimus.msu.montana.edu"
-#   common_settings
-# end
-
-namespace :deploy do 
-  run "scp ../output web_user@optimus.msu.montana.edu://var/www/yogo-test"
-  common_settings
+# Get rid of these overridden tasks if you want to use cap's native copy
+namespace :deploy do
+  desc 'Upload the site'
+  task :update_code do
+    Net::SCP.start(server_url, user, :port => 22) do |scp|
+      scp.upload! 'output',  release_path, :recursive => true
+    end
+  end
+  
+  desc "Skipping restart task"
+  task :restart do
+    puts "  * Skipping the restart task!"
+  end
 end
